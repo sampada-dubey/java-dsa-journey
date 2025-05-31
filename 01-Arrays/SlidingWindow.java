98d06a2b44fd8c64d29a6ad67b47369625b054b7
Sampada Dubey <sampadadubey41@gmail.com>
2025-05-31 17:42:35 +0530

    Added variable-size sliding window problems for Day 4:   01-Arrays/SlidingWindow.java 	Added variable-size sliding window problems for Day 4:   01-Arrays/SlidingWindow.java

--- 01-Arrays/SlidingWindow.java
+++ 01-Arrays/SlidingWindow.java
@@ -47,6 +47,49 @@ public class SlidingWindow
         return res;
     }
 
+    // 3. Longest substring with at most k distinct characters
+    static int longestKDistinct(String s, int k)
+    {
+        int left = 0, maxLen = 0;
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(int right = 0; right < s.length(); right++)
+        {
+            char c = s.charAt(right);
+            map.put(c, map.getOrDefault(c,0) + 1);
+
+            // If more than k distinct charas, shrink window
+            while(map.size() > k)
+            {
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar) - 1);
+                if(map.get(leftChar) == 0) map.remove(leftChar);
+                left++;
+            }
+
+            // Update max length
+            maxLen = Math.max(maxLen, right - left + 1); 
+        }
+        return maxLen;
+    }
+
+    // 4. Longest Subarray with sum at most k
+    static int longestSum(int[] arr, int k)
+    {
+        int left = 0, sum = 0, maxLen = 0;
+        for(int right = 0; right < arr.length; right++)
+        {
+            sum += arr[right];
+            while(sum > k)
+            {
+                sum -= arr[left];
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+
     public static void main(String[] args)
     {
         int[] arr1 = {2, 1, 5, 1, 3, 2};
@@ -56,5 +99,13 @@ public class SlidingWindow
         int[] arr2 = {12, -1, -7, 8, 15, 30, 16, 28};
         int k2 = 3;
         System.out.println("First negatives in each window: " + firstNegativeInWindow(arr2, k2));
+
+        String s = "eceba";
+        int kDistinct = 2;
+        System.out.println("Longest substring with at most " + kDistinct + " distinct: " + longestKDistinct(s, kDistinct));
+
+        int[] arr3 = {4, 2, 1, 1, 1, 2, 3};
+        int maxSumK = 5;
+        System.out.println("Longest subarray with sum at most " + maxSumK + ": " + longestSubarraySumAtMostK(arr3, maxSumK));
     }
 }
